0044f655aa83cf03134be383c61593ca6580e58c
ashugeo <hugo.saintemarie@gmail.com>
2019-12-08 18:22:41 +0100

    Add day 4

---
+++ 2019/4-2.js
@@ -0,0 +1,34 @@
+const range = [137683, 596253];
+
+const passes = [];
+
+for (let i = 0; i < range[1] - range[0]; i += 1) {
+    let rule1 = true;
+    let rule2 = false;
+    const pass = range[0] + i;
+
+    const digits = pass.toString().split('').map(d => parseInt(d));
+    for (let n in digits) {
+        n = parseInt(n);
+        if (digits[n + 1] !== undefined && digits[n + 1] < digits[n]) rule1 = false;
+
+        if (
+            (
+                digits[n + 2] === undefined &&
+                digits[n + 1] === digits[n] &&
+                digits[n - 1] !== digits[n]
+            ) || (
+                digits[n + 2] !== undefined &&
+                digits[n + 1] === digits[n] &&
+                digits[n + 2] !== digits[n] &&
+                digits[n - 1] !== digits[n]
+            )
+        ) {
+            rule2 = true;
+        }
+    }
+
+    if (rule1 && rule2) passes.push(pass);
+}
+
+console.log(passes.length);
