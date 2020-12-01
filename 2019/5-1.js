272e90edb63872975fa293da41e47f223976eebd
ashugeo <hugo.saintemarie@gmail.com>
2020-12-01 21:26:28 +0100

    Uncommitted 2019 draft

---
+++ 2019/5-1.js
@@ -0,0 +1,21 @@
+const data = require('./data.json');
+
+for (let i = 0; i < data.length; i += 4) {
+    const opcode = data[i];
+
+    if (opcode === 1) {
+        data[data[i + 3]] = data[data[i + 1]] + data[data[i + 2]];
+    } else if (opcode === 2) {
+        data[data[i + 3]] = data[data[i + 1]] * data[data[i + 2]];
+    } else if (opcode === 3) {
+
+    } else if (opcode === 4) {
+        
+    } else if (opcode === 99) {
+        return console.log(data);
+    } else {
+        console.log('error');
+    }
+}
+
+console.log(data);
