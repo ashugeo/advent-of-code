96f6377bd8b230c9582680ea884511c3d0770e49
ashugeo <hugo.saintemarie@gmail.com>
2020-12-05 14:15:32 +0100

    Day 5

---
+++ 2020/5-1.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+fs.readFile('./data.md', 'utf8', (err, data) => {
+    data = data.split('\n');
+    // console.log(data);
+
+    const seats = [];
+
+    for (const seat of data) {
+        let row = seat.substring(0, 7);
+        row = row.replace(/F/g, 0).replace(/B/g, 1);
+        row = parseInt(row, 2);
+        // console.log(row);
+
+        let col = seat.substring(7);
+        col = col.replace(/L/g, 0).replace(/R/g, 1);
+        col = parseInt(col, 2);
+        // console.log(col);
+
+        const id = row * 8 + col;
+    
+        seats.push(id);
+    }
+
+    console.log(Math.max(...seats));
+});
