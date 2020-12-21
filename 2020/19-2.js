3d4a2b141b62d4b49de64b373d4729b345222a6a
ashugeo <hugo.saintemarie@gmail.com>
2020-12-21 22:16:53 +0100

    Day 19-2

--- 2020/19-2.js
+++ 2020/19-2.js
@@ -10,41 +10,42 @@ fs.readFile('./data.md', 'utf8', (err, data) => {
         return acc;
     }, {});
 
-    // console.log(rules);
-    rules['8'] = '42 | 42 8';
-    rules['11'] = '42 31 | 42 11 31';
-
     while (Object.values(rules).filter(d => d.match(/\d/)).length > 3) {
         for (const id of Object.keys(rules)) {
             const rule = rules[id];
 
             if (!rule.match(/\d/)) {
                 for (const _id of Object.keys(rules)) {
-                    // if (_id === id) continue;
                     const _rule = rules[_id];
 
                     const re = new RegExp(`\\b${id}\\b`, 'g');
-                    rules[_id] = _rule.replace(re, `(${rule})`);
+                    rules[_id] = _rule.replace(re, `(?:${rule})`);
 
                     if (!rules[_id].match(/\d/)) rules[_id] = rules[_id].replace(/ /g, '');
-                    if (!rules[_id].match(/\d/)) rules[_id] = rules[_id].replace(/\(a\)/g, 'a');
-                    if (!rules[_id].match(/\d/)) rules[_id] = rules[_id].replace(/\(b\)/g, 'b');
+                    if (!rules[_id].match(/\d/)) rules[_id] = rules[_id].replace(/\(\?\:a\)/g, 'a');
+                    if (!rules[_id].match(/\d/)) rules[_id] = rules[_id].replace(/\(\?\:b\)/g, 'b');
                 }
             }
         }
     }
 
-    for (let i = 0; i < 5; i += 1) {
-        rules['8'] = rules['8'].replace('8', rules['8']);
-        rules['11'] = rules['11'].replace('11', rules['11']);
-    }
-
+    rules['8'] = `((?:${rules['42']})+)`;
+    rules['11'] = `((?:${rules['42']})+)((?:${rules['31']})+)`;
+    
     // console.log(rules);
 
-    const re = new RegExp(`^(${rules[8].replace('8', '')})(${rules[11].replace('11', '')})$`);
+    const re0 = new RegExp(`^${rules['8']}${rules['11']}$`);
+    const re42 = new RegExp(rules['42'], 'g');
+    const re31 = new RegExp(rules['31'], 'g');
+
+    // console.log(re0);
 
     let valid = 0;
-    for (const line of data) if (line.match(re)) valid += 1;
+
+    for (const line of data) {
+        const m = line.match(re0);
+        if (m && m[1].match(re42).length - (m[3].match(re31).length - m[2].match(re42).length) > 0) valid += 1;
+    }
 
     console.log(valid);
 });
