4dfc6f8dd7c5965235746b264d974da6de5fc8de
ashugeo <hugo.saintemarie@gmail.com>
2020-12-21 17:22:10 +0100

    [WIP] 18-2

--- 2020/18-2.js
+++ 2020/18-2.js
@@ -7,20 +7,44 @@ fs.readFile('./data.md', 'utf8', (err, data) => {
 
     let total = 0;
     for (let line of data) {
-        console.log('---');
-        console.log(line);
+        // console.log('---');
+        // console.log(line);
+
         while (isNaN(line)) {
             let pair = line.match(/\(\d+ \+ \d+\)/)?.[0];
             if (!pair) pair = line.match(/\d+ \+ \d+/)?.[0];
+
             if (!pair) pair = line.match(/\d+ \+ \([\d\* ]+\)/)?.[0];
             if (!pair) pair = line.match(/\([\d\* ]+\) \+ \d+/)?.[0];
-            if (!pair) pair = line.match(/\([\d\* ]*(\(\d+ \* \d+\))[\d\* ]*\)/)?.[0];
+            
+            if (!pair) pair = line.match(/\([\d\* ]+\) \+ \([\d\* ]+\)/)?.[0];
+
+            if (!pair) {
+                pair = line.match(/\d+ \+ [\d\*\(\) ]+|[\d\*\(\) ]+ \+ \d+/)?.[0].trim();
+
+                if (pair) {
+                    let par = -1;
+                    for (let i in pair) {
+                        if (pair[i] === '(') par = par === -1 ? 1 : ++par;
+                        if (pair[i] === ')') par -= 1;
+
+                        if (par === 0) {
+                            pair = pair.slice(0, parseInt(i) + 1);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!pair) pair = line.match(/\([\d\* ]*(\(\d+ \* \d+\))[\d\* ]*\)/)?.[0];  
             if (!pair) pair = line.match(/\d+ \* \d+/)?.[0];
             if (!pair) pair = line.match(/\(\d+\)/)?.[0];
 
-            console.log(pair);
             line = line.replace(pair, `${eval(pair)}`);
-            console.log(line);
+
+            // console.log(pair);
+            // console.log('-');
+            // console.log(line);
         }
 
         console.log(line);
@@ -35,3 +59,5 @@ fs.readFile('./data.md', 'utf8', (err, data) => {
 // 237940609397646
 // 240019893422125
 // 240450807308313
+// 241215764951770
+// 316238181417131
